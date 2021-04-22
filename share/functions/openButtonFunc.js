f72bdfb684779960731e41d71a6d0f95fe750993
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 15:48:41 +0000

    Add files via upload

---
+++ share/functions/openButtonFunc.js
@@ -0,0 +1,17 @@
+const openButtonFunc = async () => {
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localVideo.srcObject = localStream;
+        localVideo.muted = true;
+
+        remoteStream = new MediaStream();
+        remoteVideo.srcObject = remoteStream;
+
+        openButton.disabled = true;
+        createButton.disabled = false;
+        joinButton.disabled = false;
+        hangupButton.disabled = false;
+    } catch (error) {
+        console.log(error)
+    }
+}
