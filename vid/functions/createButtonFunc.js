eb5b2b39b70251bfa82a351ea0c8224d64c0053f
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 11:36:35 +0000

    Update createButtonFunc.js

--- vid/functions/createButtonFunc.js
+++ vid/functions/createButtonFunc.js
@@ -2,6 +2,31 @@ const createButtonFunc = async () => {
     await openButtonFunc();
     peerConnection = new RTCPeerConnection(configuration);
 
+peerConnection.onconnectionstatechange = ev => {
+  switch(peerConnection.connectionState) {
+    case "new":
+    case "checking":
+      console.log("Connecting...");
+      break;
+    case "connected":
+      console.log("Online");
+      break;
+    case "disconnected":
+      console.log("Disconnecting...");
+      break;
+    case "closed":
+      console.log("Offline");
+      break;
+    case "failed":
+      console.log("Error");
+      break;
+    default:
+      console.log("Unknown");
+      break;
+  }
+}
+
+
     localStream.getTracks().forEach(track => {
         peerConnection.addTrack(track, localStream)
     })
