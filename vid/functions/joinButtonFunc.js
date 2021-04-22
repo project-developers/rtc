a9ee6355ee7cd79081687e781cadd2018cc42477
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 11:47:03 +0000

    Update joinButtonFunc.js

--- vid/functions/joinButtonFunc.js
+++ vid/functions/joinButtonFunc.js
@@ -4,6 +4,30 @@ const joinButtonFunc = async () => {
 
     peerConnection = new RTCPeerConnection(configuration);
 
+peerConnection.onconnectionstatechange = ev => {
+  switch(peerConnection.connectionState) {
+    case "new":
+    case "checking":
+      console.log("Connecting...");
+      break;
+    case "connected":
+      console.log("Online");
+      break;
+    case "disconnected":
+      console.log("Disconnecting...");
+      break;
+    case "closed":
+      console.log("Offline");
+      break;
+    case "failed":
+      console.log("Error");
+      break;
+    default:
+      console.log("Unknown");
+      break;
+  }
+}
+
     const roomRef = firestore.collection("rooms").doc(roomId);
     const roomSnapshot = await roomRef.get();
 
@@ -39,33 +63,6 @@ const joinButtonFunc = async () => {
         const answer = await peerConnection.createAnswer();
         //   console.log('Created answer:', answer);
         await peerConnection.setLocalDescription(answer);
-
-
-peerConnection.onconnectionstatechange = ev => {
-  switch(peerConnection.connectionState) {
-    case "new":
-    case "checking":
-      console.log("Connecting...");
-      break;
-    case "connected":
-      console.log("Online");
-      break;
-    case "disconnected":
-      console.log("Disconnecting...");
-      break;
-    case "closed":
-      console.log("Offline");
-      break;
-    case "failed":
-      console.log("Error");
-      break;
-    default:
-      console.log("Unknown");
-      break;
-  }
-}
-
-
 
         const roomWithAnswer = {
           answer: {
