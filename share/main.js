b7fda44c226c709d22a770ac65e4f2d11edf7250
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 17:05:26 +0000

    Update main.js

--- share/main.js
+++ share/main.js
@@ -596,7 +596,7 @@ async function displayStats() {
 }
 
 function chatlog(msg) {
-  chatelement = document.getElementById('chatlog');
+  const chatelement = document.getElementById('chatlog');
   chatelement.innerHTML = '<p>[' + new Date() + '] ' + msg + '</p>';
   //chatelement.innerHTML += '<p>[' + new Date() + '] ' + msg + '</p>';
   chatelement.scrollTop = chatelement.scrollHeight
