0eeefd8126049f3f402a477230b8c1fa4c4b0615
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 15:02:29 +0000

    Add files via upload

---
+++ audio/events.js
@@ -0,0 +1,4 @@
+openButton.addEventListener("click", openButtonFunc);
+createButton.addEventListener("click", createButtonFunc);
+joinButton.addEventListener("click", joinButtonFunc);
+hangupButton.addEventListener("click", hangupButtonFunc);
