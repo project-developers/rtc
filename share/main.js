2c03865612f4acdbfa3aedcdd4dcc4b38c473e97
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 16:09:01 +0000

    Update main.js

--- share/main.js
+++ share/main.js
@@ -594,3 +594,10 @@ async function displayStats() {
     }
   }
 }
+
+function chatlog(msg) {
+  chatelement = document.getElementById('chatlog');
+  chatelement.innerHTML = '<p>[' + new Date() + '] ' + msg + '</p>';
+  //chatelement.innerHTML += '<p>[' + new Date() + '] ' + msg + '</p>';
+  chatelement.scrollTop = chatelement.scrollHeight
+}
