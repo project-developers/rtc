1e99ca34461525e8e84a2a16ef0d255b962ea658
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-22 11:31:20 +0000

    Update events.js

--- vid/events.js
+++ vid/events.js
@@ -2,3 +2,27 @@ openButton.addEventListener("click", openButtonFunc);
 createButton.addEventListener("click", createButtonFunc);
 joinButton.addEventListener("click", joinButtonFunc);
 hangupButton.addEventListener("click", hangupButtonFunc);
+
+peerConnection.onconnectionstatechange = ev => {
+  switch(peerConnection.connectionState) {
+    case "new":
+    case "checking":
+      console.log("Connecting...");
+      break;
+    case "connected":
+      console.log("Online");
+      break;
+    case "disconnected":
+      console.log("Disconnecting...");
+      break;
+    case "closed":
+      console.log("Offline");
+      break;
+    case "failed":
+      console.log("Error");
+      break;
+    default:
+      console.log("Unknown");
+      break;
+  }
+}
