92583ef380b94da82e434b1dd2250d6807417051
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-24 01:01:10 +0000

    Update openButtonFunc.js

--- vid/functions/openButtonFunc.js
+++ vid/functions/openButtonFunc.js
@@ -1,15 +1,17 @@
 const openButtonFunc = async () => {
     try {
         if(videos.innerHTML == ''){
+        videos.appendChild(localVideo);
         localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
         localVideo.srcObject = localStream;
         localVideo.muted = true;
-        videos.appendChild(localVideo);
+        //videos.appendChild(localVideo);
         }
         
+        videos.appendChild(remoteVideo);
         remoteStream = new MediaStream();
         remoteVideo.srcObject = remoteStream;
-        videos.appendChild(remoteVideo);
+        //videos.appendChild(remoteVideo);
 
         openButton.disabled = true;
         createButton.disabled = false;
