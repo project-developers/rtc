84b49e625409c9dca06a448294e635998a59852c
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-24 08:37:37 +0000

    Update createButtonFunc.js

--- vid/functions/createButtonFunc.js
+++ vid/functions/createButtonFunc.js
@@ -70,7 +70,7 @@ peerConnection.onconnectionstatechange = ev => {
         // console.log('Got remote track:', event.streams[0]);
         event.streams[0].getTracks().forEach(track => {
           // console.log('Add a track to the remoteStream:', track);
-          remoteStream[k].addTrack(track);
+          remoteStream.addTrack(track);
         })
        })
 
@@ -103,7 +103,7 @@ peerConnection.onconnectionstatechange = ev => {
     
     i++;
     //j++;
-    k++;
+    //k++;
 
       return () => {
           unsubscribe();
