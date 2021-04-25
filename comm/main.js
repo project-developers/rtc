24eac051a59e40af39789f1f87029fb1b53d8e7f
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-25 22:34:29 +0000

    Update main.js

--- comm/main.js
+++ comm/main.js
@@ -75,7 +75,7 @@ const offerOptions = {
 
 
 webcamButton.onclick = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"}, audio: true });
+  localStream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: "user"}, audio: true });
   //localStream.muted = true;
   remoteStream = new MediaStream();
   //pc = new RTCPeerConnection(servers);
@@ -84,11 +84,6 @@ webcamButton.onclick = async () => {
   localStream.getTracks().forEach((track) => {
     pc.addTrack(track, localStream);
   });
-  
-  switchCameraButton.onclick = async () => {
-  const vid = localStream.getTracks();
-  vid[1].applyConstraints({facingMode: {exact: "user"}});
-  }
   
   const [audioReceiver, videoReceiver] = pc.getReceivers();
   audioReceiver.playoutDelayHint = 0.5;
@@ -256,4 +251,7 @@ function hangup() {
   
   };
 
-
+switchCameraButton.onclick = async () => {
+  const vid = localStream.getTracks();
+  vid[1].applyConstraints({facingMode: "environment"});
+  }
