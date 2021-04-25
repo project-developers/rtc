12e55c5ba9003c9ac705449070a7805ac58126bd
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-25 16:58:41 +0000

    Update main.js

--- comm/main.js
+++ comm/main.js
@@ -85,8 +85,8 @@ webcamButton.onclick = async () => {
   });
   
   const [audioReceiver, videoReceiver] = pc.getReceivers();
-  audioReceiver.playoutDelayHint = 1.0;
-  videoReceiver.playoutDelayHint = 1.0;
+  audioReceiver.playoutDelayHint = 0.1;
+  videoReceiver.playoutDelayHint = 0.1;
 
   // Pull tracks from remote stream, add to video stream
   pc.ontrack = (event) => {
