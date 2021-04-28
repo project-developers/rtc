f3ce32f74e3282885c142ad09b97155aa269a237
project-developers <80718041+project-developers@users.noreply.github.com>
2021-04-28 12:53:46 +0000

    Update index.js

--- vid/index.js
+++ vid/index.js
@@ -105,3 +105,119 @@ localVideo.setAttribute("playsinline",null);/*
 remoteVideo[i].autoplay = true;
 remoteVideo[i].muted = true;
 remoteVideo[i].setAttribute("playsinline",null);*/
+
+var newStream = null;
+var cam = 0;
+
+switchCameraButton.onclick = async () => {
+   // example to change video camera, suppose selected value saved into window.selectedCamera
+if(cam == 0){
+ if(newStream){
+   const tracks = newStream.getTracks();
+tracks.forEach(track => track.stop());
+ }
+   newStream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment'}, audio: false }); // Or 'environment'user
+   var newVideo = newStream.getVideoTracks()[0];
+   //var videoTrack = localStream.getVideoTracks()[0];
+   var sender = pc.getSenders().find(function(s) {
+        return s.track.kind == newVideo.kind;
+      });
+      console.log('found sender:', sender);
+      sender.replaceTrack(newVideo);
+   webcamVideo.srcObject = null;
+  localStream = null;
+  localStream = newStream;
+  webcamVideo.srcObject = localStream;
+ webcamVideo.muted = true;
+  webcamVideo.play();
+  cam = 1
+}else{
+  if(newStream){
+   const tracks = newStream.getTracks();
+tracks.forEach(track => track.stop());
+ }
+newStream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: 'user'}, audio: false }); // Or 'environment'user
+  var newVideo = newStream.getVideoTracks()[0];
+   //var videoTrack = localStream.getVideoTracks()[0];
+   var sender = pc.getSenders().find(function(s) {
+        return s.track.kind == newVideo.kind;
+      });
+      console.log('found sender:', sender);
+      sender.replaceTrack(newVideo);
+   webcamVideo.srcObject = null;
+  localStream = null;
+  localStream = newStream;
+  webcamVideo.srcObject = localStream;
+ webcamVideo.muted = true;
+  webcamVideo.play();
+  cam = 0
+}
+ }
+ 
+/*  
+  // example to change video camera, suppose selected value saved into window.selectedCamera
+switchCameraButton.onclick = async () => {
+navigator.mediaDevices
+  .getUserMedia({
+    video: {
+      facingMode: 'environment'
+    }
+  })
+  .then(function(stream) {
+  webcamVideo.srcObject = null;
+  webcamVideo.srcObject = stream;
+    let videoTrack = stream.getVideoTracks()[0];
+ //   PCs.forEach(function(pc) {
+      var sender = pc.getSenders().find(function(s) {
+        return s.track.kind == videoTrack.kind;
+      });
+      console.log('found sender:', sender);
+      sender.replaceTrack(videoTrack);
+   // });
+  })
+  .catch(function(err) {
+    console.error('Error happens:', err);
+  });
+  
+}
+ */
+ //create button to toggle video
+var video_button = document.getElementById("cameraButton");
+//video_button.appendChild(document.createTextNode("Toggle hold"));
+video_button.onclick = function(){
+  if(video_button.innerText == "Camera Off"){
+    video_button.innerText = "Camera On"
+  }else{
+  video_button.innerText = "Camera Off"
+  };
+  localStream.getVideoTracks()[0].enabled = !(localStream.getVideoTracks()[0].enabled);
+  webcamVideo.srcObject = null;
+  webcamVideo.srcObject = localStream;
+  webcamVideo.muted = true;
+  webcamVideo.play();
+}
+
+ var audio_button = document.getElementById("muteButton");
+ //video_button.appendChild(document.createTextNode("Toggle hold"));
+
+ 
+ audio_button.onclick = function(){
+   if(audio_button.innerText == "Mute"){
+     audio_button.innerText = "Unmute"
+   }else{
+   audio_button.innerText = "Mute"
+   };
+   localStream.getAudioTracks()[0].enabled = !(localStream.getAudioTracks()[0].enabled);
+  
+   /*webcamVideo.srcObject = null;
+  webcamVideo.srcObject = localStream;
+   webcamVideo.muted = true;
+  webcamVideo.play();*/
+ }
+   /*
+   audio_button.onclick = function(evt) {
+   const newState = !localStream.getAudioTracks()[0].enabled;
+   audio_button.innerHTML = newState ? "&#x25B6;&#xFE0F;" : "&#x23F8;&#xFE0F;";
+   localStream.getAudioTracks()[0].enabled = newState;
+ }
+  */
